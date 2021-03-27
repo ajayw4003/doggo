ca6ae217337f9ed1adfd599311095efa2c5e3c10
ajayw4003 <ajayw4003@gmail.com>
2021-03-27 19:47:00 +0530

    doggo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
 
+import {useState, useEffect} from 'react';
+import "./App.css";
 export default function App() {
+  const [dogs, setDogs] = useState({
+    message:{}
+  });
+  const [dogName, setDogName] = useState("")
+  const [img, setImg] = useState("https://images.dog.ceo/breeds/affenpinscher/n02110627_2416.jpg")
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(result =>{
+      // console.log(result);
+      setDogs(result);
+    })
+    return () => {
+      
+    };
+  }, [])
+
+  const handleChange = (e) =>{
+    const seletedDog = e.target.value;
+    setDogName(seletedDog);
+    fetch(`https://dog.ceo/api/breed/${seletedDog}/images/random`)
+    .then(response => response.json())
+    .then(result =>{
+      // console.log(result);
+      setImg(result.message);
+
+    })
+  }
 
   return (
-    <div className="App">
-	  <h1>hello world</h1>
+    <div className="app">
+      <h1>Doggo</h1>
+	    
+      <select className ="select" onChange = {handleChange}>
+        {Object.keys(dogs.message).map((dog) =>(
+          <option key = {dog} value = {dog}>{dog}</option>
+        ))}
+      </select>
+      <h1>Get Image Here</h1>
+      <div className ="img">
+          <img src = {img} alt = {`no image for ${dogName}`}/>
+      </div>
+      
     </div>
   );
 }
